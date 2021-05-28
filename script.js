bac62c6d238fd81ced9a50c6975b75bd455d419a
anthonyzzin <80277056+anthonyzzin@users.noreply.github.com>
2021-05-28 12:13:59 -0300

    Desafio 5 em JS
    Vai na Web

---
+++ script.js
@@ -0,0 +1,17 @@
+let itens = ["Trini", "Tommy", "Kim", "Zack", "Jason"]
+console.log (itens)
+
+itens.unshift("Power")
+
+itens.pop()
+
+itens.push("Mighty", "Morphin")
+
+itens.shift()
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort(function (a, b){
+    return a-b
+});
+console.log(numbers)
